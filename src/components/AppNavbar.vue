d18962f6ba955e1dbc386d695da429007d0a25fa
Juan <juanfajardoreyes@hotmail.com>
2025-04-05 16:21:05 +0200

    Feat: Login/Dashboard y componentes AppFooter, AppNavar AppHeader creados, falta el css/Bootstrpa

---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,37 @@
+<script lang="ts">
+export default {
+  name: 'AppNavbar',
+};
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <router-link to="/" class="navbar-brand">Inicio</router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+            <router-link to="/dashboard" class="nav-link">Dashboard</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/patients" class="nav-link">Pacientes</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/login" class="nav-link">Cerrar Sesión</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import PatientInfo from '@/pages/PatientInfo.vue'
 import Login from '@/pages/LoginPage.vue'
-import Dashboard from '@/pages/DashboardPage.vue'
+//import { useAuthStore } from '@/stores/auth' Para autenticacion
+import DashboardPage from '@/pages/DashboardPage.vue'
+import PatientInfo from '@/pages/PatientInfo.vue'
 
 const routes = [
-  { path: '/', component: Login },
-  { path: '/dashboard', component: Dashboard },
-  { path: '/patient/:id', component: PatientInfo, props: true },
+  {
+    path: '/',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardPage,
+  },
+  {
+    path: '/patient/:id',
+    name: 'PatientInfo',
+    component: PatientInfo,
+  }
 ]
-
 const router = createRouter({
   history: createWebHistory(),
   routes,
