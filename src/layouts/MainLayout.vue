e334f7f685888f0d0c6765b3673e0ba72ce81153
Juan <juanfajardoreyes@hotmail.com>
2025-04-05 14:17:30 +0200

    Fix: Cambios en ficheros de configuración para hacer funcionar al decorador @ de las rutas

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="layout">
+    <Header />
+    <Navbar />
+
+    <main class="flex-grow 1">
+      <slot /> <!-- Aquí se carga el contenido de cada página -->
+    </main>
+
+    <Footer />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+import Navbar from '@/components/Navbar.vue'
+</script>
+
+<style scoped>
+.layout {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+main {
+  flex: 1;
+}
+</style>
